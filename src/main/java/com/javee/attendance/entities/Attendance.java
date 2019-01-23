a2cdf6d408549a69d6c171c39833b21c621dc2bd
dipesh <dipesh_gandhi9@yahoo.com>
2019-01-23 10:52:32 +0530

    Security Added for the attendance api endpoints

--- src/main/java/com/javee/attendance/entities/Attendance.java
+++ src/main/java/com/javee/attendance/entities/Attendance.java
@@ -3,55 +3,65 @@ package com.javee.attendance.entities;
 import javax.persistence.*;
 import java.util.Date;
 
-
 @Entity
-public class Attendance {
+public class Attendance
+{
 	@Id
 	@GeneratedValue( strategy = GenerationType.IDENTITY )
 	private Long id;
 	@ManyToOne
 	private Employee employee;
 	private Date timeIn;
-	private Date timeOut;	
+	private Date timeOut;
 
-	protected Attendance() {
+	protected Attendance()
+	{
 	}
 
-	public Attendance( Employee employee, Date timeIn, Date timeOut ){
+	public Attendance( Employee employee, Date timeIn, Date timeOut )
+	{
 		this.employee = employee;
 		this.timeIn = timeIn;
 		this.timeOut = timeOut;
 	}
-	
-	 public Long getId() {
-	        return id;
+
+	public Long getId()
+	{
+		return id;
 	}
-	 
-	 public void setId( Long id ){
-	        this.id = id;
+
+	public void setId( Long id )
+	{
+		this.id = id;
 	}
-	 
-	 public Employee getEmployee() {
-		 return employee;
-	 }
-	 
-	 public void setEmployee( Employee employee) {
-		 this.employee = employee;
-	 }
-	 
-	 public Date getTimeInDate() {
-		 return timeIn;
-	 }
-	 
-	 public void setTimeInDate( Date timeIn) {
-		 this.timeIn = timeIn;
-	 }
-	 
-	 public Date geTimeOuttDate() {
-		 return timeOut;
-	 }
-	 
-	 public void setTimeOutDate( Date timeOut) {
-		 this.timeOut = timeOut;
-	 }
+
+	public Employee getEmployee()
+	{
+		return employee;
+	}
+
+	public void setEmployee( Employee employee )
+	{
+		this.employee = employee;
+	}
+
+	public Date getTimeInDate()
+	{
+		return timeIn;
+	}
+
+	public void setTimeInDate( Date timeIn )
+	{
+		this.timeIn = timeIn;
+	}
+
+	public Date geTimeOuttDate()
+	{
+		return timeOut;
+	}
+
+	public void setTimeOutDate( Date timeOut )
+	{
+		this.timeOut = timeOut;
+	}
 }
